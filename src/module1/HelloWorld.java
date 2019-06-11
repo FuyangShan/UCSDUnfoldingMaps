e5343d10558f33ba82c342f9e1aa91ca43bdac26
Fuyang_Shan <fuyang.shan@gmail.com>
2019-06-11 14:53:29 -0700

    Add a second map from hometown

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -76,6 +76,9 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 400, 50, 350, 500, provider);
+		
+		map2.zoomAndPanTo(zoomLevel, new Location(41.8f,123.6f));
 
 	}
 
@@ -84,6 +87,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
